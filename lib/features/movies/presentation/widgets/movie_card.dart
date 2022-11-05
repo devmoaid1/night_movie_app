ca6e0d3b696d01c05088c5c80ef5d10bc7d707d7
devmoaid1 <moaidmohamed1312@gmail.com>
2022-11-06 01:26:09 +0200

    app image

--- lib/features/movies/presentation/widgets/movie_card.dart
+++ lib/features/movies/presentation/widgets/movie_card.dart
@@ -1,8 +1,8 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/constants/app_constants.dart';
+import '../../../../core/widgets/app_image.dart';
 import '../../domain/entites/movie.dart';
 
 class MovieCard extends StatelessWidget {
@@ -12,25 +12,22 @@ class MovieCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        // navigate to product details page
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.amber,
-        ),
-        width: 100.w,
-        margin: EdgeInsets.only(right: 5.w, bottom: 5.h),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: ExtendedImage.network(
-            AppConstants.baseImageUrl(movie.backdropPath),
-            cache: true,
-            fit: BoxFit.cover,
+        onTap: () {
+          // navigate to product details page
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.amber,
           ),
-        ),
-      ),
-    );
+          width: 100.w,
+          margin: EdgeInsets.only(right: 5.w, bottom: 5.h),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: AppImage(
+                height: MediaQuery.of(context).size.height * 0.55,
+                path: AppConstants.baseImageUrl(movie.backdropPath),
+              )),
+        ));
   }
 }
--- lib/features/movies/presentation/widgets/now_playing_movie_card.dart
+++ lib/features/movies/presentation/widgets/now_playing_movie_card.dart
@@ -1,8 +1,8 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/constants/app_constants.dart';
+import '../../../../core/widgets/app_image.dart';
 import '../../domain/entites/movie.dart';
 
 class NowPlayingMovieCard extends StatelessWidget {
@@ -13,30 +13,27 @@ class NowPlayingMovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(key: UniqueKey(), children: [
       ShaderMask(
-        shaderCallback: (rect) {
-          return const LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              // fromLTRB
-              Colors.transparent,
-              Colors.black,
-              Colors.black,
-              Colors.transparent,
-            ],
-            stops: [0, 0.3, 0.5, 1],
-          ).createShader(
-            Rect.fromLTRB(0, 0, rect.width, rect.height),
-          );
-        },
-        blendMode: BlendMode.dstIn,
-        child: ExtendedImage.network(
-          AppConstants.baseImageUrl(movie.backdropPath),
-          cache: true,
-          fit: BoxFit.cover,
-          height: MediaQuery.of(context).size.height * 0.55,
-        ),
-      ),
+          shaderCallback: (rect) {
+            return const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                // fromLTRB
+                Colors.transparent,
+                Colors.black,
+                Colors.black,
+                Colors.transparent,
+              ],
+              stops: [0, 0.3, 0.5, 1],
+            ).createShader(
+              Rect.fromLTRB(0, 0, rect.width, rect.height),
+            );
+          },
+          blendMode: BlendMode.dstIn,
+          child: AppImage(
+            height: MediaQuery.of(context).size.height * 0.55,
+            path: AppConstants.baseImageUrl(movie.backdropPath),
+          )),
       Align(
           alignment: Alignment.bottomCenter,
           child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
