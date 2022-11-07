3f681724aed5221bd1f85f0e86982337c8ef01da
devmoaid1 <moaidmohamed1312@gmail.com>
2022-11-07 20:56:30 +0200

    completed movie details repository

--- lib/features/movies/data/datasources/movie_details_remote_datasource.dart
+++ lib/features/movies/data/datasources/movie_details_remote_datasource.dart
@@ -1,4 +1,6 @@
 import 'package:night_movie_app/core/api/api_provider.dart';
+import 'package:night_movie_app/features/movies/data/models/movie_details_model.dart';
+import 'package:night_movie_app/features/movies/data/models/recommended_movie_model.dart';
 import 'package:night_movie_app/features/movies/domain/entites/movie_details.dart';
 import 'package:night_movie_app/features/movies/domain/entites/recommended_movie.dart';
 
@@ -20,15 +22,23 @@ class MovieDetailsDataRemoteDataSource implements MovieDetailsDataSource {
     try {
       final result =
           await apiProvider.getData(AppConstants.movieDetailsEndpoint(id));
-      return const MovieDetails();
+      return MovieDetailsModel.fromJson(result);
     } on ServerException catch (err) {
       throw ServerException(errorModel: err.errorModel);
     }
   }
 
   @override
-  Future<List<RecommendedMovie>> getRecommendedMovies({required String id}) {
-    // TODO: implement getRecommendedMovies
-    throw UnimplementedError();
+  Future<List<RecommendedMovie>> getRecommendedMovies(
+      {required String id}) async {
+    try {
+      final result =
+          await apiProvider.getData(AppConstants.recommendedMoviesEndpoint(id));
+      return List.from(result['results'])
+          .map((result) => RecommendedMovieModel.fromJson(result))
+          .toList();
+    } on ServerException catch (err) {
+      throw ServerException(errorModel: err.errorModel);
+    }
   }
 }
---
+++ lib/features/movies/data/repositories/movie_details_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:night_movie_app/core/errors/exceptions.dart';
+import 'package:night_movie_app/features/movies/data/datasources/movie_details_remote_datasource.dart';
+import 'package:night_movie_app/features/movies/domain/entites/recommended_movie.dart';
+import 'package:night_movie_app/features/movies/domain/entites/movie_details.dart';
+import 'package:night_movie_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:night_movie_app/features/movies/domain/repositories/movie_details_repository.dart';
+
+class MovieDetailsRepositoryImpl implements MovieDetilsRepository {
+  final MovieDetailsDataSource movieDetailsDataSource;
+
+  MovieDetailsRepositoryImpl({required this.movieDetailsDataSource});
+
+  @override
+  Future<Either<Failure, MovieDetails>> getMovieDetails(
+      {required String id}) async {
+    try {
+      final response = await movieDetailsDataSource.getMovieDetails(id: id);
+      return Right(response);
+    } on ServerException catch (err) {
+      debugPrint(err.errorModel.statusMessage);
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<RecommendedMovie>>> getMovieRecommendedMovies(
+      {required String id}) async {
+    try {
+      final response =
+          await movieDetailsDataSource.getRecommendedMovies(id: id);
+      return Right(response);
+    } on ServerException catch (err) {
+      debugPrint(err.errorModel.statusMessage);
+      return Left(ServerFailure());
+    }
+  }
+}
